b5d33b535c836e0c09bdf0f29c7fff5cfd6039fc
tarcisiobm <tarcisiobmf16@gmail.com>
2025-06-10 13:44:08 -0300

    initial commit - backend

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,8 +1,22 @@
 <script setup lang="ts">
+import api from '@/plugins/axios'
+import { ref } from 'vue';
+const message = ref('');
+const pingServer = async () => {
+  try {
+    const response = await api.get('/ping');
+    console.log(response);
+    message.value = response.data.message
+  } catch (error) {
+    console.error('erro na requisição:', error);
+  }
+}
 </script>
 
 <template>
   <main>
     <h1>Hello World</h1>
+    <v-btn @click="pingServer">ping</v-btn>
+    <div v-if="message">{{ message }}</div>
   </main>
 </template>
