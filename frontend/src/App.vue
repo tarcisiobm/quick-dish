c58a430d4eb6cb855698080f5fb7fbcd48be776b
tarcisiobm <tarcisiobmf16@gmail.com>
2025-06-10 12:11:50 -0300

    initial commit: frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <nav class="d-flex align-baseline">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/login">Login</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style lang="scss" scoped>
+</style>
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about">
+    <h1>Login</h1>
+    <v-text-field label="login" width="400"></v-text-field>
+    <v-text-field label="password" width="400"></v-text-field>
+  </div>
+</template>
+
+<style lang="scss">
+</style>
